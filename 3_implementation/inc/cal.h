15d4e2a51508d2e4215b11371d9f49441cdb4fdb
Charu-20 <101492174+Charu-20@users.noreply.github.com>
2022-03-30 11:23:41 +0530

    Add files via upload

---
+++ 3_implementation/inc/cal.h
@@ -0,0 +1,27 @@
+/**
+ * @file cal.h
+ * @author Amol Kulkarni (amolkulkarni000@gmail.com)
+ * @brief Mathematical operations of calculator
+ * @version 0.1
+ * @date 2022-03-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+//Include files
+#include <stdio.h>    
+#include <math.h>  
+#include <stdlib.h>  
+#include"fAct.c"
+#include"mOdulus.c"
+      
+// function declarations   
+float addition();  
+float subtract();  
+float multiply();  
+float divide();  
+int mOdulus();
+int fAct();  
+
