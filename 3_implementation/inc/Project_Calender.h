e6c806c5b763db55987a5b97376fb7da9d8511ae
Charu-20 <101492174+Charu-20@users.noreply.github.com>
2022-03-30 11:23:01 +0530

    Delete Project_Calender.h

--- 3_implementation/inc/Project_Calender.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJECT_CALENDER_H
-#define PROJECT_CALENDER_H
-
-int dayNumber(int day, int month, int year);
-char* getMonthName(int monthNumber);
-int numberOfDays(int monthNumber, int year);
-void printCalendar(int year);
-
-#endif
